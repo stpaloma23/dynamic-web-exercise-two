0b654244baac440d32472f47497a63ad4685e56c
Paloma St.Clair <palomast.clair@10-18-17-194.dynapool.wireless.nyu.edu>
2022-09-20 09:39:12 -0400

    setting up router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
+import List from "./pages/list";
+import Aritcles from "./pages/article";
+import { ReactDOM } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider, 
+  Route,
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <List />,
+  },
+  {
+    path: "/article",
+    element: <Aritcles />,
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <RouterProvider router={router} />
     </div>
   );
 }
